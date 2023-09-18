573ba793c9b7807ed4d13b60b32c54bf51433fd9
AhmadAzizi30 <ahmadazizi@student.ub.ac.id>
2023-09-18 19:01:32 +0700

    ini commit pertama

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        purple: "#6C3077",
+        pink: "#A73284",
+        orange: "#EC5A2C",
+      },
+      fontFamily: {
+        dsd: ["DM Serif Display"],
+        manrope: ["manrope"],
+      },
+      screens: {
+        "2xl": "1350px",
+      },
+    },
+  },
+  plugins: [],
+};
