f157f23f2d92fd4afc9ae6018b725d8583947508
nsywang <nasywaanggita21@gmail.com>
2023-09-27 14:13:55 +0700

    edit

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-let sliderContainer = document.getElementById('sliderContainer');
-let slider = document.getElementById('slider');
-let cards = slider.getElementsByTagName('li');
+let sliderContainer = document.getElementById("sliderContainer");
+let slider = document.getElementById("slider");
+let cards = slider.getElementsByTagName("li");
 
 let elementsToShow = 3;
 
 let sliderContainerWidth = sliderContainer.clientWidth;
 
-let cardWidth = sliderContainerWidth/elementsToShow;
+let cardWidth = sliderContainerWidth / elementsToShow;
 
-slider.style.width=cards.length*cardWidth+'px';
+slider.style.width = cards.length * cardWidth + "px";
 
-for(let index =0; index < cards.length; index++){
+for (let index = 0; index < cards.length; index++) {
   const element = cards[index];
-  element.style.width=cardWidth+'px';
+  element.style.width = cardWidth + "px";
 }
-function prev(){
-    if(+slider.style.marginLeft.slice(0,-2)!= -cardWidth*(cards.length - elementsToShow))
-    slider.style.marginLeft=((+slider.style.marginLeft.slice(0,-2))- cardWidth)+'px'//100px
+function prev() {
+  if (
+    +slider.style.marginLeft.slice(0, -2) !=
+    -cardWidth * (cards.length - elementsToShow)
+  )
+    slider.style.marginLeft =
+      +slider.style.marginLeft.slice(0, -2) - cardWidth + "px"; //100px
 }
-function next(){
-    if(+slider.style.marginLeft.slice(0,-2)!=0)
-    slider.style.marginLeft=((+slider.style.marginLeft.slice(0,-2))+cardWidth)+'px'//100px
-
+function next() {
+  if (+slider.style.marginLeft.slice(0, -2) != 0)
+    slider.style.marginLeft =
+      +slider.style.marginLeft.slice(0, -2) + cardWidth + "px"; //100px
 }
