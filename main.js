e1fe40feae7947f02cd6d5c012d1a79846e2a505
nsywang <nasywaanggita21@gmail.com>
2023-09-29 23:09:26 +0700

    edit

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ let sliderContainer = document.getElementById("sliderContainer");
 let slider = document.getElementById("slider");
 let cards = slider.getElementsByTagName("li");
 
-
-
 let elementsToShow = 3;
-if(document.body.clientWidth <1000){
-  elementsToShow=1;
-}else if(document.body.clientWidth<1500){
-  elementsToShow=2;
+if (document.body.clientWidth < 1000) {
+  elementsToShow = 1;
+} else if (document.body.clientWidth < 1500) {
+  elementsToShow = 2;
 }
 
 let sliderContainerWidth = sliderContainer.clientWidth;
@@ -17,38 +15,43 @@ let sliderContainerWidth = sliderContainer.clientWidth;
 let cardWidth = sliderContainerWidth / elementsToShow;
 
 slider.style.width = cards.length * cardWidth + "px";
-slider.style.transition='margin';
-slider.style.transitionDuration='1s';
-
+slider.style.transition = "margin";
+slider.style.transitionDuration = "1s";
 
 for (let index = 0; index < cards.length; index++) {
   const element = cards[index];
   element.style.width = cardWidth + "px";
 }
 
-function prev(){
-  if(+slider.style.marginLeft.slice(0,-2)!=-cardWidth*(cards.length-elementsToShow)){
-  slider.style.marginLeft = ((+slider.style.marginLeft.slice(0,-2))-cardWidth)+'px';
-  firstVisibleIndex++;
+function prev() {
+  if (
+    +slider.style.marginLeft.slice(0, -2) !=
+    -cardWidth * (cards.length - elementsToShow)
+  ) {
+    slider.style.marginLeft =
+      +slider.style.marginLeft.slice(0, -2) - cardWidth + "px";
+    firstVisibleIndex++;
   }
 }
-function next(){
-  if(+slider.style.marginLeft.slice(0,-2)!=0){
-  slider.style.marginLeft = ((+slider.style.marginLeft.slice(0,-2))+cardWidth)+'px';
-  firstVisibleIndex--;
+function next() {
+  if (+slider.style.marginLeft.slice(0, -2) != 0) {
+    slider.style.marginLeft =
+      +slider.style.marginLeft.slice(0, -2) + cardWidth + "px";
+    firstVisibleIndex--;
   }
 }
 function prev() {
   if (firstVisibleIndex > 0) {
-    slider.style.marginLeft = ((+slider.style.marginLeft.slice(0, -2)) + cardWidth) + 'px';
+    slider.style.marginLeft =
+      +slider.style.marginLeft.slice(0, -2) + cardWidth + "px";
     firstVisibleIndex--;
   }
 }
 
 function next() {
   if (firstVisibleIndex < cards.length - elementsToShow) {
-    slider.style.marginLeft = ((+slider.style.marginLeft.slice(0, -2)) - cardWidth) + 'px';
+    slider.style.marginLeft =
+      +slider.style.marginLeft.slice(0, -2) - cardWidth + "px";
     firstVisibleIndex++;
   }
 }
-
